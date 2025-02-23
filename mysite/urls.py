72eee4fa5d077feb24739e98991d25ffc92efd5f
Niclas Svanström <niclas.svanstrom@hotmail.com>
2025-02-23 08:18:24 +0100

    test

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,17 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from chatbot_app.views import chat_page
 
+from settings import DEBUG
 from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import path, include
+from django.conf.urls.static import static, serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', chat_page, name='home'),
     path('chat/', include('chatbot_app.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
